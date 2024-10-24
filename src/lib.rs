e5cea38bd7b6d78fc92df7f71d31cc270d306368
grey <greysdevmail@gmail.com>
2024-10-24 16:23:23 +0200

    Cargo.toml: Add example, features, etc.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 pub use fyfth_core as core;
+#[cfg(feature = "focus")]
 pub use fyfth_focus as focus;
+#[cfg(feature = "egui_terminal")]
 pub use fyfth_terminal as terminal;
 
 pub mod prelude {
     pub use fyfth_core::*;
+    #[cfg(feature = "focus")]
     pub use fyfth_focus::*;
+    #[cfg(feature = "egui_terminal")]
     pub use fyfth_terminal::*;
 }
